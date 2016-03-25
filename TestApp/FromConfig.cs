9101d0cedbba9e59a43b0cf654ca5015de486abc
Avinash Sachan <avinashsachan@users.noreply.github.com>
2016-03-25 10:12:43 +0530

    Added files via upload

---
+++ TestApp/FromConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class FromConfig
+    {
+        public static string Server { get {
+            return System.Configuration.ConfigurationManager.AppSettings["Server"].ToString();
+
+        
+        } }
+
+        public static string Username
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings["Username"].ToString();
+
+
+            }
+        }
+
+        public static string Password
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings["Password"].ToString();
+
+
+            }
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelnetLib;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new connection 
+            var con = new TelnetClient(FromConfig.Server, 23, 30);
+
+            if (con.Connect())
+            {
+                Console.WriteLine("Conencted");
+                //System.Threading.Thread.Sleep(2000);
+                con.WaitFor("login:");
+                con.SendAndWait(FromConfig.Username + "\n", "Password:",true);
+                con.SendAndWait(FromConfig.Password + "\n", "$", true);
+                Console.WriteLine(con.SessionLog);
+                con.SendAndWait("ls -lrt" + "\n", "$", true);
+            }
+            else
+            {
+                Console.WriteLine("No connection.");
+            }
+
+
+
+
+        }
+    }
+}
